80812a1fc48ab80a5ccf97933832affadcfbd32d
Anton <dzanto@gmail.com>
2020-11-10 14:32:56 +0300

    select and event_loop done

--- select_and_event_loop.py
+++ select_and_event_loop.py
@@ -12,43 +12,46 @@ server_socket.bind(('localhost', 5000))
 server_socket.listen()
 
 def accept_connection(server_socket):
-    while True:
-        print('Before .accept()')
-        # accept an incoming connection - принять входящиее подключение
-        # accept блокирующая функция, ждет входящее подключение
-        client_socket, addr = server_socket.accept()
-        print('Connection from', addr)
-        send_message(client_socket)
+    print('Before .accept()')
+    # accept an incoming connection - принять входящиее подключение
+    # accept блокирующая функция, ждет входящее подключение
+    # после подключения создаем client_socket
+    client_socket, addr = server_socket.accept()
+    print('Connection from', addr)
+
+    # добавляем client_socket для select
+    to_monitor.append(client_socket)
 
 
 def send_message(client_socket):
-    while True:
-        print('Before .recv()')
-        # await receiving a message from a client
-        request = client_socket.recv(4096)
-        decode_request = request.decode('utf-8')
-        print('Client request = ', decode_request)
+    print('Before .recv()')
+    # await receiving a message from a client
+    request = client_socket.recv(4096)
+    decode_request = request.decode('utf-8')
+    print('Client request = ', decode_request)
 
-        if not request:
-            print('No data')
-            break
-        else:
-            response = 'Hello world\r\n'.encode()
-            # send a message to a client
-            client_socket.send(response)
-
-    # close connection after a disconnecting by the client
-    print('Disconnect')
-    client_socket.close()
+    if request:
+        response = 'Hello world\r\n'.encode()
+        # send a message to a client
+        client_socket.send(response)
+    else:
+        # close connection
+        print('Disconnect')
+        client_socket.close()
 
 
 def event_loop():
     while True:
-        ready_to_read, _, _ = select(to_monitor, [], [])
+        ready_to_read, _, _ = select(to_monitor, [], []) # read, write, error
+        for sock in ready_to_read:
+            if sock is server_socket:
+                accept_connection(sock)
+            else:
+                send_message(sock)
 
 
 if __name__ == '__main__':
     to_monitor.append(server_socket)
-    # accept_connection(server_socket)
+    event_loop()
 
 # command for telnet: open servadr port. For example: open localhost 5000
